8937be9e8d96c02e1f266c66c7fd9d5c80b6431d
cowboys24 <169158486+cowboys24@users.noreply.github.com>
2024-07-02 04:37:52 +0200

    Added Account.java and AccountFactory.java (#13)
    - Implemented Account class (POJO) with attributes and methods.
    - Created AccountFactory class to generate instances of Account.
    
    24 June 2024
    Musaddiq McWhite (219369151)

--- src/main/java/za/ac/cput/campusconnect/domain/Account.java
+++ src/main/java/za/ac/cput/campusconnect/domain/Account.java
@@ -1,11 +1,98 @@
 package za.ac.cput.campusconnect.domain;
 
-/**
- * FileName.java
- * Class:
- * Author:
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+/**S
+ * Account.java
+ * Class: Account
+ * Author: Musaddiq McWhite (219369151)
  * Completion date:
  */
 
+@Entity
 public class Account {
+    @Id
+    private String accountNumber;
+    private String username;
+    private String password;
+    private String userType;
+
+    public Account() {}
+
+    public Account(Builder builder) {
+        this.accountNumber = builder.accountNumber;
+        this.username = builder.username;
+        this.password = builder.password;
+        this.userType = builder.userType;
+    }
+    public String getAccountNumber() {return accountNumber;}
+
+    public String getUsername() {return username;}
+
+    public String getPassword() {return password;}
+
+    public String getUserType() {return userType;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(accountNumber, account.accountNumber) && Objects.equals(username, account.username) && Objects.equals(password, account.password) && Objects.equals(userType, account.userType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, username, password, userType);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", userType='" + userType + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String accountNumber;
+        private String username;
+        private String password;
+        private String userType;
+
+        public Builder setAccountNumber(String accountNumber) {
+            this.accountNumber = accountNumber;
+            return this;
+        }
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setUserType(String userType) {
+            this.userType = userType;
+            return this;
+        }
+
+        public Builder copy(Account account) {
+            this.accountNumber = account.accountNumber;
+            this.username = account.username;
+            this.password = account.password;
+            this.userType = account.userType;
+            return this;
+        }
+
+        public Account build() {return new Account(this);}
+    }
+
 }
