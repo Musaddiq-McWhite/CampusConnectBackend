cfd2aed0539c0c97dfd17d2a9599f97bae62bf4f
Daany34 <220094934@mycput.ac.za>
2024-07-09 18:42:41 +0200

    Added in Room POJO and factory

--- src/main/java/za/ac/cput/campusconnect/factory/RoomFactory.java
+++ src/main/java/za/ac/cput/campusconnect/factory/RoomFactory.java
@@ -1,5 +1,9 @@
 package za.ac.cput.campusconnect.factory;
 
+import za.ac.cput.campusconnect.domain.Property;
+import za.ac.cput.campusconnect.domain.Room;
+import za.ac.cput.campusconnect.util.Helper;
+
 /**
  * FileName.java
  * Class:
@@ -9,4 +13,9 @@ package za.ac.cput.campusconnect.factory;
 
 public class RoomFactory {
 
+    public static Room buildRoom(String roomID, Property property, int roomNumber, boolean occupied){
+        if(Helper.isNullOrEmpty(roomID) || Helper.isNullOrEmpty(property) || Helper.isNullOrEmpty(roomNumber) || Helper.isNullOrEmpty(occupied))
+            return null;
+        return new Room.Builder().setRoomID(roomID).setProperty(property).setRoomNumber(roomNumber).setOccupied(occupied).build();
+    }
 }
