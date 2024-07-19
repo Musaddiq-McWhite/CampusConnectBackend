e044bb446057cfd489b0a4068057cff7e8d10d2e
Kabo Khudunyane <khudunyane64@gmail.com>
2024-07-19 15:26:47 +0200

    Update StudentController.java

--- src/main/java/za/ac/cput/campusconnect/controller/StudentController.java
+++ src/main/java/za/ac/cput/campusconnect/controller/StudentController.java
@@ -1,11 +1,56 @@
 package za.ac.cput.campusconnect.controller;
 
-/**
- * FileName.java
- * Class:
- * Author:
- * Completion date:
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Student;
+import za.ac.cput.service.StudentService;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        Student createdStudent = studentService.create(student);
+        return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/read/{studentID}")
+    public ResponseEntity<Student> getStudent(@PathVariable String studentID) {
+        Student student = studentService.read(studentID);
+        if (student != null) {
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student) {
+        Student updatedStudent = studentService.update(student);
+        return new ResponseEntity<>(updatedStudent, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{studentID}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable String studentID) {
+        studentService.delete(studentID);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> studentList = studentService.getAllStudents();
+        return new ResponseEntity<>(studentList, HttpStatus.OK);
+    }
 }
