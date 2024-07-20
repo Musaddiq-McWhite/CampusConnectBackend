73a5b082a2729c8938b2ea47d1031d0c11244d6e
Aphelele218130260 <218130260@mycput.ac.za>
2024-07-20 02:47:16 +0200

    repository and service for business and property

--- src/main/java/za/ac/cput/campusconnect/service/IPropertyService.java
+++ src/main/java/za/ac/cput/campusconnect/service/IPropertyService.java
@@ -1,11 +1,17 @@
 package za.ac.cput.campusconnect.service;
 
+import za.ac.cput.campusconnect.domain.Property;
+
+import java.util.List;
+
 /**
  * FileName.java
  * Interface:
- * Author:
+ * Author:Aphelele Zimkhita Joka 218130260
  * Completion date:
  */
 
-public interface IPropertyService {
+public interface IPropertyService  extends IService<Property, String>
+{
+    List<Property> getall();
 }
