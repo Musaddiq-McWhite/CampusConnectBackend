adbd08a74761bd26350628daa257a59c07a0e624
madavhashirly <127310079+madavhashirly@users.noreply.github.com>
2024-07-20 11:04:48 +0200

    factory and repo (#25)
    * domain class for administrator
    
    * hey i am sending request for admin factory

--- src/main/java/za/ac/cput/campusconnect/factory/AdministratorFactory.java
+++ src/main/java/za/ac/cput/campusconnect/factory/AdministratorFactory.java
@@ -1,5 +1,9 @@
 package za.ac.cput.campusconnect.factory;
 
+
+import za.ac.cput.campusconnect.domain.Administrator;
+import za.ac.cput.campusconnect.util.Helper;
+
 /**
  * FileName.java
  * Class:
@@ -8,4 +12,9 @@ package za.ac.cput.campusconnect.factory;
  */
 
 public class AdministratorFactory {
+    public static Administrator buildAdministrator(int adminId, String adminUsername, String adminPassword) {
+        if (Helper.isNullOrEmpty(adminId) || Helper.isNullOrEmpty(adminUsername) || Helper.isNullOrEmpty(adminPassword) )
+            return null;
+        return new Administrator.Builder().setAdminId(adminId).setAdminUsername(adminUsername).setAdminPassword(adminPassword).build();
+    }
 }
