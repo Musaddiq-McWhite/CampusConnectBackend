0bd65a2183acc0a6bb1489024cd3cd20975ec84e
cowboys24 <169158486+cowboys24@users.noreply.github.com>
2024-07-20 13:17:42 +0200

    Added Repository for Account (#29)
    * Added Account.java and AccountFactory.java
    
    - Implemented Account class (POJO) with attributes and methods.
    - Created AccountFactory class to generate instances of Account.
    
    24 June 2024
    Musaddiq McWhite (219369151)
    
    * Added tests for AccountFactory
    
    25 June 2024
    Musaddiq McWhite (219369151)
    
    * Added missing Factory and Service Tests
    - AdministratorFactoryTest
    - AdministratorServiceTest
    - BusinessFactoryTest
    - BusinessServiceTest
    - PropertyFactoryTest
    - PropertyServiceTest
    
    Cowboys24 (219369151)
    
    * Added Repository for Account
    - AccountRepository
    
    (219369151 - cowboys24)

--- src/main/java/za/ac/cput/campusconnect/repository/AccountRepository.java
+++ src/main/java/za/ac/cput/campusconnect/repository/AccountRepository.java
@@ -1,11 +1,24 @@
 package za.ac.cput.campusconnect.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.campusconnect.domain.Account;
+
+import java.util.List;
+import java.util.Set;
+
 /**
- * FileName.java
- * Interface:
- * Author:
+ * AccountRepository.java
+ * Interface: AccountRepository
+ * Author: Musaddiq McWhite (219369151 - Cowboys24)
  * Completion date:
  */
 
-public interface AccountRepository {
+
+public interface AccountRepository extends JpaRepository<Account, String> {
+
+        public Account findAccountByAccountNumber(String accountNumber);
+        Set<Account> findAllByUserType(String userType);
+        void deleteAccountByAccountNumber(String accountNumber);
+        List<Account> findAll();
+
 }
